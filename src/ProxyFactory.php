28eb4cfe4fe09419fec8acf1f32b5668b31a756b
Stratadox <info@stratadox.com>
2019-08-08 16:36:48 +0200

    Add exception interface for when proxy production fails

--- src/ProxyFactory.php
+++ src/ProxyFactory.php
@@ -13,8 +13,9 @@ interface ProxyFactory
     /**
      * Creates a new proxy.
      *
-     * @param array $knownData Associative array with the known data.
-     * @return Proxy           The proxy, sherlock.
+     * @param array $knownData       Associative array with the known data.
+     * @return Proxy                 The proxy, sherlock.
+     * @throws ProxyProductionFailed When the proxy could not be created.
      */
     public function create(array $knownData = []): Proxy;
 }
---
+++ src/ProxyProductionFailed.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Stratadox\Proxy;
+
+use Throwable;
+
+/**
+ * Exception to throw when the proxy could not be created.
+ *
+ * @author Stratadox
+ */
+interface ProxyProductionFailed extends Throwable
+{
+}
