6536b58d7d7cf96e84aa4d9f22c62eeb6d3c275b
Stratadox <info@stratadox.com>
2019-08-08 18:35:27 +0200

    Remove constructor requirement form interface

--- src/Proxy.php
+++ src/Proxy.php
@@ -9,13 +9,4 @@ namespace Stratadox\Proxy;
  */
 interface Proxy
 {
-    /**
-     * Proxies are given a loader and an associative array of known data.
-     *
-     * @param ProxyLoader $loader The loader responsible for fetching the real
-     *                            object when any of the proxy methods is called.
-     * @param array$knownData     Associative array containing what information
-     *                            we have on the represented object.
-     */
-    public function __construct(ProxyLoader $loader, array $knownData);
 }
